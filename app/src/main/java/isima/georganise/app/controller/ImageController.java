28c4d54af7d99b7fe330245f966c6994cadab363
Yukimyo <thuthur1er@gmail.com>
2024-02-23 14:54:56 +0100

    Added exception management

--- app/src/main/java/isima/georganise/app/controller/ImageController.java
+++ app/src/main/java/isima/georganise/app/controller/ImageController.java
@@ -1,6 +1,7 @@
 package isima.georganise.app.controller;
 
 import isima.georganise.app.entity.dao.Image;
+import isima.georganise.app.exception.UnimplementedException;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -12,26 +13,26 @@ public class ImageController {
 
     @GetMapping(path="/{id}", produces = "application/json")
     public ResponseEntity<Image> getImageById(@PathVariable("id") Long id) {
-        return null;
+        throw new UnimplementedException();
     }
 
     @GetMapping(path="/keyword/{keyword}", produces = "application/json")
     public ResponseEntity<List<Image>> getImagesByKeyword(@PathVariable("keyword") String keyword) {
-        return null;
+        throw new UnimplementedException();
     }
 
     @PostMapping(path="/", consumes = "application/json", produces = "application/json")
     public ResponseEntity<Image> createImage(@RequestBody Image image) {
-        return null;
+        throw new UnimplementedException();
     }
 
     @PutMapping(path="/{id}", consumes = "application/json", produces = "application/json")
     public ResponseEntity<Image> updateImage(@PathVariable("id") Long id, @RequestBody Image image) {
-        return null;
+        throw new UnimplementedException();
     }
 
     @DeleteMapping(path="/{id}")
     public ResponseEntity<Void> deleteImage(@PathVariable("id") Long id) {
-        return null;
+        throw new UnimplementedException();
     }
 }
