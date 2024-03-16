e51c2e8dff8fb1c13e1bcfb8c23da2e334966af7
Yukimyo <thuthur1er@gmail.com>
2024-03-16 20:05:23 +0100

    change image format in database

--- app/src/main/java/isima/georganise/app/service/image/ImageService.java
+++ app/src/main/java/isima/georganise/app/service/image/ImageService.java
@@ -2,6 +2,7 @@ package isima.georganise.app.service.image;
 
 import isima.georganise.app.entity.dao.Image;
 import isima.georganise.app.entity.dto.ImageCreationDTO;
+import isima.georganise.app.entity.dto.ImageDTO;
 import isima.georganise.app.entity.dto.ImageUpdateDTO;
 import org.springframework.stereotype.Service;
 
@@ -10,16 +11,16 @@ import java.util.UUID;
 @Service
 public interface ImageService {
 
-    Iterable<Image> getAllImages(UUID authToken);
+    Iterable<ImageDTO> getAllImages(UUID authToken);
 
-    Image getImageById(UUID authToken, Long id);
+    ImageDTO getImageById(UUID authToken, Long id);
 
-    Iterable<Image> getImageByKeyword(UUID authToken, String keyword);
+    Iterable<ImageDTO> getImageByKeyword(UUID authToken, String keyword);
 
-    Image createImage(UUID authToken, ImageCreationDTO image);
+    ImageDTO createImage(UUID authToken, ImageCreationDTO image);
 
     void deleteImage(UUID authToken, Long id);
 
-    Image updateImage(UUID authToken, Long id, ImageUpdateDTO imageUpdateDTO);
+    ImageDTO updateImage(UUID authToken, Long id, ImageUpdateDTO imageUpdateDTO);
 
 }
