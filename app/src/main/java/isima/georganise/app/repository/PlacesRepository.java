1510d56aae9ce2912c38002ea0e8e2e15f2f97fd
Yukimyo <thuthur1er@gmail.com>
2024-03-17 22:30:57 +0100

    fix join on placeByKeyword

--- app/src/main/java/isima/georganise/app/repository/PlacesRepository.java
+++ app/src/main/java/isima/georganise/app/repository/PlacesRepository.java
@@ -19,7 +19,7 @@ public interface PlacesRepository extends JpaRepository<Place, Long> {
     @Query("SELECT p FROM Place p JOIN PlaceTag pt ON pt.place.placeId = p.placeId WHERE pt.tag.tagId = :id")
     List<Place> findByTagId(Long id);
 
-    @Query("SELECT p FROM Place p JOIN PlaceTag pt ON pt.place.placeId = p.placeId WHERE UPPER(p.name) LIKE UPPER(CONCAT('%', :keyword, '%')) OR UPPER(p.description) LIKE UPPER(CONCAT('%', :keyword, '%')) OR UPPER(pt.tag.title) LIKE UPPER(CONCAT('%', :keyword, '%')) OR UPPER(pt.tag.description) LIKE UPPER(CONCAT('%', :keyword, '%'))")
+    @Query("SELECT p FROM Place p LEFT JOIN PlaceTag pt ON pt.place.placeId = p.placeId LEFT JOIN Tag t ON pt.tag.tagId = t.tagId WHERE UPPER(p.name) LIKE UPPER(CONCAT('%', :keyword, '%')) OR UPPER(p.description) LIKE UPPER(CONCAT('%', :keyword, '%')) OR UPPER(pt.tag.title) LIKE UPPER(CONCAT('%', :keyword, '%')) OR UPPER(pt.tag.description) LIKE UPPER(CONCAT('%', :keyword, '%'))")
     List<Place> findByKeyword(String keyword);
 
     @Query("SELECT p FROM Place p JOIN PlaceTag pt ON pt.place.placeId = p.placeId JOIN Token t ON pt.tag.tagId = t.tagId WHERE p.latitude BETWEEN :minLatitude AND :maxLatitude AND p.longitude BETWEEN :minLongitude AND :maxLongitude AND (t.userId = :userId OR p.userId = :userId)")
