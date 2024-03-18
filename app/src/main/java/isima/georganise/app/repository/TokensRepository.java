3e4220b1bbf07b0546e7d86c2ef9cbccb90510f6
Yukimyo <thuthur1er@gmail.com>
2024-03-18 23:06:21 +0100

    change addTokenToUser to use token value instead of id

--- app/src/main/java/isima/georganise/app/repository/TokensRepository.java
+++ app/src/main/java/isima/georganise/app/repository/TokensRepository.java
@@ -6,6 +6,7 @@ import org.springframework.stereotype.Repository;
 
 import java.util.List;
 import java.util.Optional;
+import java.util.UUID;
 
 @Repository
 public interface TokensRepository extends JpaRepository<Token, Long> {
@@ -17,4 +18,6 @@ public interface TokensRepository extends JpaRepository<Token, Long> {
     List<Token> findByUserIdAndTagId(Long userId, Long tagId);
 
     List<Token> findByTagId(Long tagId);
+
+    Optional<Token> findByTokenValue(UUID token);
 }
